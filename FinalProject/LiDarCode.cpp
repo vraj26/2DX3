5ee4ed5db9f7a80c7af78abf3c70a233ee2adebd
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-05-26 13:09:34 -0400

    Update LiDarCode.cpp

--- FinalProject/LiDarCode.cpp
+++ FinalProject/LiDarCode.cpp
@@ -1,26 +1,3 @@
-/*  Time of Flight for 2DX4 -- Studio W8-0
-                Code written to support data collection from VL53L1X using the Ultra Light Driver.
-                I2C methods written based upon MSP432E4 Reference Manual Chapter 19.
-                Specific implementation was based upon format specified in VL53L1X.pdf pg19-21
-                Code organized according to en.STSW-IMG009\Example\Src\main.c
-                
-                The VL53L1X is run with default firmware settings.
-
-
-            Written by Tom Doyle
-            Updated by  Hafez Mousavi Garmaroudi
-            Last Update: March 17, 2020
-						
-						Last Update: March 03, 2022
-						Updated by Hafez Mousavi
-						__ the dev address can now be written in its original format. 
-								Note: the functions  beginTxI2C and  beginRxI2C are modified in vl53l1_platform_2dx4.c file
-								
-						Modified March 16, 2023 
-						by T. Doyle
-							- minor modifications made to make compatible with new Keil IDE
-
-*/
 #include <stdint.h>
 #include "PLL.h"
 #include "SysTick.h"
